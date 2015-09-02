961bc2a0a4794c2caf103e50ab526e54598e225f
ignacioalv <ignacioalv@hotmail.com>
2015-09-02 10:57:32 -0300

    Config locationProvider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 'use strict';
 var wingestFront = angular.module("wingestFront", ["ngRoute"]).
-  config(function($routeProvider){
+  config(function($routeProvider, $locationProvider){
     $routeProvider.
       when('/',{
-        templateUrl: '/views/main.html',
+        templateUrl: '/views/main.html'
       }).
       when('/contacto',{
-        templateUrl: '/views/contacto.html',
+        templateUrl: '/views/contacto.html'
       })
+
+     $locationProvider.html5Mode(true);
   });
